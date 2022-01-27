8a60dfb0633e726c4a71e2b5845e857070ce280e
Kat <51405063+ArchiGen@users.noreply.github.com>
2022-01-27 19:36:25 +0100

    HTML and CSS added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Weather />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Weather />
+
+      <footer>
+        <small>
+          Open-source by
+          <a
+            href="https://github.com/ArchiGen/weather-app-she-codes"
+            target="_blank"
+          >
+            {" "}
+            ArchiGen
+          </a>
+        </small>
+      </footer>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,9 +1,51 @@
 import React from "react";
+import "./Weather.css";
 
 export default function Weather() {
   return (
-    <div>
-      <h2>Hello weather</h2>
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Where?"
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
+      </form>
+      <h1>City City</h1>
+      <ul>
+        <li>Wenesday 10:00</li>
+        <li>Mostly Cloudly</li>
+      </ul>
+
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/snow.png"
+            alt="snow icon"
+          />
+          <span className="temperature">6</span>{" "}
+          <span className="unit">°C</span>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Percipitation: 14%</li>
+            <li>Humidity: 72%</li>
+            <li>Wind: 5m/s</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
