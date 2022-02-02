f7cea01e71201828e0605ce759dc5f25c96654df
Kat <51405063+ArchiGen@users.noreply.github.com>
2022-02-02 13:37:23 +0100

    Bug in WeatherForecast fixed
    Correct forecast loading on refresh

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import "./WeatherForecast.css";
 import axios from "axios";
@@ -8,30 +8,45 @@ export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecastData, setForecastData] = useState(false);
 
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coords]);
+
   function handleResponse(response) {
     //console.log(response.data);
     setForecastData(response.data.daily);
     setLoaded(true);
   }
 
-  if (loaded) {
-    console.log(forecastData);
-    return (
-      <div className="WeatherForecast">
-        <div className="row">
-          <div className="col-2">
-            <WeatherForecastDay data={forecastData[0]} />
-          </div>
-        </div>
-      </div>
-    );
-  } else {
+  function load() {
     let apiKey = "8faec0e45e93e61e880c60b71662661c";
     let lon = props.coords.lon;
     let lat = props.coords.lat;
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
+  }
 
+  if (loaded) {
+    //console.log(forecastData);
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          {forecastData.map(function (dailyForecast, index) {
+            if (index < 6) {
+              return (
+                <div className="col-2" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            } else {
+              return null;
+            }
+          })}
+        </div>
+      </div>
+    );
+  } else {
+    load();
     return null;
   }
 }
